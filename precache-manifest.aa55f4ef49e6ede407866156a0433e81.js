4e1766bf61ee07d695306a72684d11a558dbbc17
Pavlo <pavloterleckij2@gmail.com>
2020-04-17 21:56:31 +0300

    Updates

---
+++ precache-manifest.aa55f4ef49e6ede407866156a0433e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5f6b6d90fcf5f7090dabb6d674c2d9",
+    "url": "/cursor-react-styled_components/index.html"
+  },
+  {
+    "revision": "f47698663a8a365958d9",
+    "url": "/cursor-react-styled_components/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "682d30ebb93b1973a896",
+    "url": "/cursor-react-styled_components/static/js/2.4cd7c9e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-react-styled_components/static/js/2.4cd7c9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47698663a8a365958d9",
+    "url": "/cursor-react-styled_components/static/js/main.5b638684.chunk.js"
+  },
+  {
+    "revision": "19f1e204ba8cf46de39b",
+    "url": "/cursor-react-styled_components/static/js/runtime-main.ddd9bbf3.js"
+  }
+]);
